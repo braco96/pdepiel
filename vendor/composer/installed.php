c98d5f9c6f9560ff452c079fd227fb0b70dfbc26
braco96 <bravocolladoluis@gmail.com>
2025-05-07 18:30:08 +0200

    n1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'braco96/pdepiel',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9825b8f8e41487eb4961f768e5168e7c6a7955d8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'braco96/pdepiel' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9825b8f8e41487eb4961f768e5168e7c6a7955d8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
